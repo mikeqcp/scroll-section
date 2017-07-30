5c7a831a42d2531bf3abd7094a07c72bfedb6588
Michał Przyszczypkowski <michal@unit9.com>
2017-07-30 12:37:01 +0200

    Scroll section component

---
+++ lib/demo.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Section from './scroll-section.jsx';
+
+export default class Demo extends Component {
+  renderSection(content, id) {
+    return (
+      <Section
+        className="section"
+        key={id}
+        onBecomeActive={() => {
+          console.log(`${id} become active`)
+        }}
+      >
+        {content}
+      </Section>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {Array.from(Array(5).keys()).map(this.renderSection)}
+      </div>
+    );
+  }
+}
--- lib/scroll-section.jsx
+++ lib/scroll-section.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
-import './scroll-section.scss';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import scrollTo from 'scroll-to';
+import throttle from 'lodash.throttle';
 
+export default class ScrollSection extends Component {
+  static propTypes = {
+    children: PropTypes.any,
+    className: PropTypes.string,
+    onBecomeActive: PropTypes.func,
+  };
 
-export default React.createClass({
-  render: function() {
-    return <div className="scroll-section">Hello World</div>;
+  componentDidMount() {
+    this.onScroll();
+    window.addEventListener('scroll', this.onScroll);
   }
-});
+
+  scrollIntoView() {
+    const topPos = this.rootEl.offsetTop;
+    scrollTo(0, topPos, {
+      ease: 'in-out-quad',
+      duration: 1000,
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  onScroll = throttle(() => {
+    const bd = this.rootEl.getBoundingClientRect();
+    const screenMiddle = window.innerHeight / 2;
+    const isActive = (bd.top < screenMiddle) && (bd.top + bd.height > screenMiddle);
+
+    if (isActive && !this.isActive) {
+      this.props.onBecomeActive();
+    }
+    this.isActive = isActive;
+  }, 100);
+
+  render() {
+    const { className } = this.props;
+    return (
+      <div className={className} ref={el => (this.rootEl = el)}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
